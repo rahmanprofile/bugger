ef8e37a597ffe7a7c99d28e4aa4428824a44ec81
Savita Nandan <nandan.savita@cris.org.in>
2025-04-24 16:33:22 +0530

    sjdgheuigh

--- lib/generator/provider/provider.dart
+++ lib/generator/provider/provider.dart
@@ -8,7 +8,7 @@ class Provider {
   static final Provider instance = Provider._privateConstructor();
 
   Future<http.Response> insert({required BuggerModel payload}) async {
-    final response = await http.post(Uri.parse(''),
+    final response = await http.post(Uri.parse('http://172.16.22.68:5000/save'),
       body: jsonEncode(payload.toJson()),
       headers: { "Content-Type": "application/json" }
     );
